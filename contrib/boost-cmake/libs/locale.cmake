2f823421b4fbfe3cd51853d410c5594ea2dc4390
liubangchen <liubangchen@tencent.com>
2022-06-23 18:00:31 +0800

    fix fmt build for velox

--- contrib/boost-cmake/libs/locale.cmake
+++ contrib/boost-cmake/libs/locale.cmake
@@ -5,7 +5,8 @@ else()
 endif()
 
 if(USE_FREEBSD)
-  try_compile(HAVE_FREEBSD_XLOCALE
+  try_compile(
+    HAVE_FREEBSD_XLOCALE
     "${CMAKE_CURRENT_BINARY_DIR}"
     "${BOOST_SOURCE}/libs/locale/build/has_xlocale.cpp"
   )
@@ -23,7 +24,7 @@ option(BOOST_LOCALE_ENABLE_STD_BACKEND "Enable STD backend in Boost Locale if av
 option(BOOST_LOCALE_ENABLE_POSIX_BACKEND "Enable Posix backend in Boost Locale if available" ${BOOST_LOCALE_ENABLE_POSIX_BACKEND_DEFAULT})
 
 if(BOOST_LOCALE_ENABLE_ICU_BACKEND)
-  find_package(ICU COMPONENTS uc dt i18n)
+  #find_package(ICU COMPONENTS uc dt i18n)
 endif()
 
 if(BOOST_LOCALE_ENABLE_ICONV_BACKEND)
@@ -38,20 +39,20 @@ endif()
 _add_boost_lib(
   NAME locale
   SOURCES
-    ${ICU_SRCS}
-    ${BOOST_SOURCE}/libs/locale/src/encoding/codepage.cpp
-    ${BOOST_SOURCE}/libs/locale/src/shared/date_time.cpp
-    ${BOOST_SOURCE}/libs/locale/src/shared/format.cpp
-    ${BOOST_SOURCE}/libs/locale/src/shared/formatting.cpp
-    ${BOOST_SOURCE}/libs/locale/src/shared/generator.cpp
-    ${BOOST_SOURCE}/libs/locale/src/shared/ids.cpp
-    ${BOOST_SOURCE}/libs/locale/src/shared/localization_backend.cpp
-    ${BOOST_SOURCE}/libs/locale/src/shared/message.cpp
-    ${BOOST_SOURCE}/libs/locale/src/shared/mo_lambda.cpp
-    ${BOOST_SOURCE}/libs/locale/src/util/codecvt_converter.cpp
-    ${BOOST_SOURCE}/libs/locale/src/util/default_locale.cpp
-    ${BOOST_SOURCE}/libs/locale/src/util/info.cpp
-    ${BOOST_SOURCE}/libs/locale/src/util/locale_data.cpp
+  ${ICU_SRCS}
+  ${BOOST_SOURCE}/libs/locale/src/encoding/codepage.cpp
+  ${BOOST_SOURCE}/libs/locale/src/shared/date_time.cpp
+  ${BOOST_SOURCE}/libs/locale/src/shared/format.cpp
+  ${BOOST_SOURCE}/libs/locale/src/shared/formatting.cpp
+  ${BOOST_SOURCE}/libs/locale/src/shared/generator.cpp
+  ${BOOST_SOURCE}/libs/locale/src/shared/ids.cpp
+  ${BOOST_SOURCE}/libs/locale/src/shared/localization_backend.cpp
+  ${BOOST_SOURCE}/libs/locale/src/shared/message.cpp
+  ${BOOST_SOURCE}/libs/locale/src/shared/mo_lambda.cpp
+  ${BOOST_SOURCE}/libs/locale/src/util/codecvt_converter.cpp
+  ${BOOST_SOURCE}/libs/locale/src/util/default_locale.cpp
+  ${BOOST_SOURCE}/libs/locale/src/util/info.cpp
+  ${BOOST_SOURCE}/libs/locale/src/util/locale_data.cpp
 )
 
 # Convenience interface library to link deps to both main library and tests
@@ -59,7 +60,8 @@ add_library(Boost_locale_deps INTERFACE)
 target_link_libraries(Boost_locale PRIVATE Boost_locale_deps)
 
 if(BOOST_LOCALE_ENABLE_ICU_BACKEND AND ICU_FOUND)
-  target_sources(Boost_locale PRIVATE
+  target_sources(
+    Boost_locale PRIVATE
     ${BOOST_SOURCE}/libs/locale/src/icu/boundary.cpp
     ${BOOST_SOURCE}/libs/locale/src/icu/codecvt.cpp
     ${BOOST_SOURCE}/libs/locale/src/icu/collator.cpp
@@ -70,17 +72,19 @@ if(BOOST_LOCALE_ENABLE_ICU_BACKEND AND ICU_FOUND)
     ${BOOST_SOURCE}/libs/locale/src/icu/numeric.cpp
     ${BOOST_SOURCE}/libs/locale/src/icu/time_zone.cpp
   )
-  target_link_libraries(Boost_locale_deps INTERFACE
+  target_link_libraries(
+    Boost_locale_deps INTERFACE
     Boost::thread
-    ICU::dt
-    ICU::i18n
-    ICU::uc
+    _icuc
+    _icudata
+    _icui18n
   )
   target_compile_definitions(Boost_locale_deps INTERFACE BOOST_LOCALE_WITH_ICU=1)
 endif()
 
 if(BOOST_LOCALE_ENABLE_STD_BACKEND)
-  target_sources(Boost_locale PRIVATE
+  target_sources(
+    Boost_locale PRIVATE
     ${BOOST_SOURCE}/libs/locale/src/std/codecvt.cpp
     ${BOOST_SOURCE}/libs/locale/src/std/collate.cpp
     ${BOOST_SOURCE}/libs/locale/src/std/converter.cpp
@@ -92,14 +96,16 @@ else()
 endif()
 
 if(BOOST_LOCALE_ENABLE_ICONV_BACKEND AND ICONV_FOUND)
-  target_link_libraries(Boost_locale_deps INTERFACE
+  target_link_libraries(
+    Boost_locale_deps INTERFACE
     Iconv::Iconv
   )
   target_compile_definitions(Boost_locale_deps INTERFACE BOOST_LOCALE_WITH_ICONV=1)
 endif()
 
 if(BOOST_LOCALE_ENABLE_WINAPI_BACKEND)
-  target_sources(Boost_locale PRIVATE
+  target_sources(
+    Boost_locale PRIVATE
     ${BOOST_SOURCE}/libs/locale/src/win32/collate.cpp
     ${BOOST_SOURCE}/libs/locale/src/win32/converter.cpp
     ${BOOST_SOURCE}/libs/locale/src/win32/numeric.cpp
@@ -110,7 +116,8 @@ else()
 endif()
 
 if(BOOST_LOCALE_ENABLE_POSIX_BACKEND)
-  target_sources(Boost_locale PRIVATE
+  target_sources(
+    Boost_locale PRIVATE
     ${BOOST_SOURCE}/libs/locale/src/posix/collate.cpp
     ${BOOST_SOURCE}/libs/locale/src/posix/converter.cpp
     ${BOOST_SOURCE}/libs/locale/src/posix/numeric.cpp
@@ -122,15 +129,19 @@ else()
 endif()
 
 if(USE_WINDOWS AND (BOOST_LOCALE_ENABLE_WINAPI_BACKEND OR BOOST_LOCALE_ENABLE_STD_BACKEND))
-  target_sources(Boost_locale PRIVATE
+  target_sources(
+    Boost_locale PRIVATE
     ${BOOST_SOURCE}/libs/locale/src/win32/lcid.cpp
   )
 endif()
 
-if(BOOST_LOCALE_ENABLE_POSIX_BACKEND
+if(
+  BOOST_LOCALE_ENABLE_POSIX_BACKEND
   OR BOOST_LOCALE_ENABLE_STD_BACKEND
-  OR BOOST_LOCALE_ENABLE_WINAPI_BACKEND)
-  target_sources(Boost_locale PRIVATE
+  OR BOOST_LOCALE_ENABLE_WINAPI_BACKEND
+)
+  target_sources(
+    Boost_locale PRIVATE
     ${BOOST_SOURCE}/libs/locale/src/util/gregorian.cpp
   )
 endif()
@@ -138,41 +149,41 @@ endif()
 _add_boost_test(
   NAME locale_test
   LINK
-    Boost::locale
-    Boost_locale_deps
+  Boost::locale
+  Boost_locale_deps
   TESTS
-    # Configuration information
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_config.cpp
-
-    # Shared
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_utf.cpp
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_date_time.cpp
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_ios_prop.cpp
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_codecvt.cpp
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_codepage_converter.cpp
-    # RUN ${BOOST_SOURCE}/libs/locale/test/test_codepage.cpp
-    # RUN ${BOOST_SOURCE}/libs/locale/test/test_message.cpp
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_generator.cpp
-
-    # icu
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_collate.cpp
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_convert.cpp
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_boundary.cpp
-    # RUN ${BOOST_SOURCE}/libs/locale/test/test_formatting.cpp
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_icu_vs_os_timezone.cpp
-
-    # winapi
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_winapi_collate.cpp
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_winapi_convert.cpp
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_winapi_formatting.cpp
-
-    # posix
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_posix_collate.cpp
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_posix_convert.cpp
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_posix_formatting.cpp
-
-    # std
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_std_collate.cpp
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_std_convert.cpp
-    RUN ${BOOST_SOURCE}/libs/locale/test/test_std_formatting.cpp
+  # Configuration information
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_config.cpp
+  
+  # Shared
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_utf.cpp
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_date_time.cpp
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_ios_prop.cpp
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_codecvt.cpp
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_codepage_converter.cpp
+  # RUN ${BOOST_SOURCE}/libs/locale/test/test_codepage.cpp
+  # RUN ${BOOST_SOURCE}/libs/locale/test/test_message.cpp
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_generator.cpp
+  
+  # icu
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_collate.cpp
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_convert.cpp
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_boundary.cpp
+  # RUN ${BOOST_SOURCE}/libs/locale/test/test_formatting.cpp
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_icu_vs_os_timezone.cpp
+  
+  # winapi
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_winapi_collate.cpp
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_winapi_convert.cpp
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_winapi_formatting.cpp
+  
+  # posix
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_posix_collate.cpp
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_posix_convert.cpp
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_posix_formatting.cpp
+  
+  # std
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_std_collate.cpp
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_std_convert.cpp
+  RUN ${BOOST_SOURCE}/libs/locale/test/test_std_formatting.cpp
 )
