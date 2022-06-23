019427d553a7c61495498b5f51f2dbf8750607b2
liubangchen <liubangchen@tencent.com>
2022-06-23 11:32:56 +0800

    build succ antlr4

--- src/demo/AntlrDemo.cpp
+++ src/demo/AntlrDemo.cpp
@@ -1,6 +1,14 @@
 
 #include <iostream>
+#include "antlr4-runtime.h"
+//#include "TLexer.h"
+//#include "TParser.h"
+
+//using namespace antlrcpptest;
+using namespace antlr4;
 
 int main(int argc, char *argv[]){
+    //ANTLRInputStream input(u8"🍴 = 🍐 + \"😎\";(((x * π))) * µ + ∰; a + (x * (y ? 0 : 1) + z);");
+
     return 0;
 }
