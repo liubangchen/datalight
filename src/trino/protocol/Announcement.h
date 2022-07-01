e3c8f310bb1a8d8d61440ee223a36c499b70d64a
liubangchen <liubangchen@tencent.com>
2022-07-01 16:43:19 +0800

    添加jwt支持

--- src/trino/protocol/Announcement.h
+++ src/trino/protocol/Announcement.h
@@ -28,26 +28,32 @@ namespace datalight::protocol
     }
     proxygen::HTTPMessage announcementRequest(const std::string & body)
     {
+        std::string token0 = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxODc1MmQzZS02NjY3LTQ3MmQtYWMxZC1lNmM5MWJiODdmNTciLCJleHAiOjE2NTY1ODc3MjV9.z1sPA-Ln-cxpLQ6L9htWn_9YZsSbEKOQ0pL_7qvI6ro";
+        auto decoded = jwt::decode(token0);
+
+        for(auto& e : decoded.get_payload_claims())
+            std::cout << e.first << " = " << e.second << std::endl;
+
         const auto time = jwt::date::clock::now();
         auto token = jwt::create()
-            .set_type("JWS")
-            .set_payload_claim("sub", jwt::claim(uuid))
+            //.set_type("JWS")
+            .set_subject(uuid)
             .set_expires_at(time + min{5})
-            //.set_payload_claim("exp",picojson::value(time+min{5}))
-            .sign(jwt::algorithm::hs256{"secret"});
+            .sign(jwt::algorithm::hs256{"production"});
         ////std::cout <<token << " = ================"  << std::endl;
         auto decoded1 = jwt::decode(token);
         for(auto& e : decoded1.get_payload_claims())
                 std::cout << e.first << " * " << e.second << std::endl;
-
+        LOG(INFO) << token ;
         proxygen::HTTPMessage request;
         request.setMethod(proxygen::HTTPMethod::PUT);
+        request.setHTTPVersion(1 , 1);
         request.setURL(fmt::format("/v1/announcement/{}", uuid));
         request.getHeaders().set("User-Agent", uuid);
         request.getHeaders().set(proxygen::HTTP_HEADER_HOST, fmt::format("{}:{}", "172.19.254.10", "9000"));
         request.getHeaders().set(proxygen::HTTP_HEADER_CONTENT_TYPE, "application/json");
-        request.getHeaders().set("X-Trino-User", "hadoop");
-        request.getHeaders().set("X-Trino-Internal-Bearer", token);
+        request.getHeaders().rawSet("X-Trino-User", "hadoop");
+        request.getHeaders().rawSet("X-Trino-Internal-Bearer", token);
         request.getHeaders().set(proxygen::HTTP_HEADER_CONTENT_LENGTH, std::to_string(body.size()));
         return request;
     }
