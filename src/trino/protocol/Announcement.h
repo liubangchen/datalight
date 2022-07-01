ec87400d7e53e1a849ce175d6d2712c1cbe5bce3
liubangchen <liubangchen@tencent.com>
2022-07-01 15:24:24 +0800

    添加jwt支持

--- src/trino/protocol/Announcement.h
+++ src/trino/protocol/Announcement.h
@@ -2,16 +2,19 @@
 #include <fmt/core.h>
 #include <nlohmann/json.hpp>
 #include <proxygen/httpserver/RequestHandler.h>
+#include <jwt/jwt.h>
 
 namespace datalight::protocol
 {
+    static const std::string uuid = "e4901aae-a5c9-9ff7-97a9-5687835ad54c";
+    using min = std::chrono::minutes;
     std::string createAnnouncementBody()
     {
         nlohmann::json body
             = {{"environment", "production"},
                {"pool", "general"},
-               {"nodeId", "e4901aae-a5c9-9ff7-97a9-5687835ad54c"},
-               {"location", "/e4901aae-a5c9-9ff7-97a9-5687835ad54c"},
+               {"nodeId", uuid},
+               {"location", fmt::format("/{}", uuid)},
                {"services",
                 {{{"id", "66b459fd-9262-4236-867d-5b0958570e2d"},
                   {"type", "trino"},
@@ -25,12 +28,26 @@ namespace datalight::protocol
     }
     proxygen::HTTPMessage announcementRequest(const std::string & body)
     {
+        const auto time = jwt::date::clock::now();
+        auto token = jwt::create()
+            .set_type("JWS")
+            .set_payload_claim("sub", jwt::claim(uuid))
+            .set_expires_at(time + min{5})
+            //.set_payload_claim("exp",picojson::value(time+min{5}))
+            .sign(jwt::algorithm::hs256{"secret"});
+        ////std::cout <<token << " = ================"  << std::endl;
+        auto decoded1 = jwt::decode(token);
+        for(auto& e : decoded1.get_payload_claims())
+                std::cout << e.first << " * " << e.second << std::endl;
+
         proxygen::HTTPMessage request;
         request.setMethod(proxygen::HTTPMethod::PUT);
-        request.setURL(fmt::format("/v1/announcement/{}", "e4901aae-a5c9-9ff7-97a9-5687835ad54c"));
-        request.getHeaders().set(proxygen::HTTP_HEADER_HOST, fmt::format("{}:{}", "172.19.254.10", "9100"));
+        request.setURL(fmt::format("/v1/announcement/{}", uuid));
+        request.getHeaders().set("User-Agent", uuid);
+        request.getHeaders().set(proxygen::HTTP_HEADER_HOST, fmt::format("{}:{}", "172.19.254.10", "9000"));
         request.getHeaders().set(proxygen::HTTP_HEADER_CONTENT_TYPE, "application/json");
         request.getHeaders().set("X-Trino-User", "hadoop");
+        request.getHeaders().set("X-Trino-Internal-Bearer", token);
         request.getHeaders().set(proxygen::HTTP_HEADER_CONTENT_LENGTH, std::to_string(body.size()));
         return request;
     }
