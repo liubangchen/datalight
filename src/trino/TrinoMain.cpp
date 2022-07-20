58ed1b13c2a4dc6370d1a062e42dfedeb79cf823
学东栾 <luanxuedong2009@gmail.com>
2022-07-20 15:42:56 +0800

    fix code style

--- src/trino/TrinoMain.cpp
+++ src/trino/TrinoMain.cpp
@@ -3,21 +3,19 @@
 #include <gflags/gflags_declare.h>
 #include <glog/logging.h>
 #include <server/TrinoServer.h>
-#include <hash-library/sha256.h>
 #include "velox/common/base/StatsReporter.h"
 
 DEFINE_string(etc_dir, ".", "etc directory for presto configuration");
 
-int main(int argc, char * argv[])
-{
-    folly::init(&argc, &argv);
-    datalight::server::TrinoServer server(FLAGS_etc_dir);
-    server.run();
-    LOG(INFO) << "SHUTDOWN: Exiting main()";
-}
+int main(int argc, char* argv[]) {
+  folly::init(&argc, &argv);
 
+  datalight::server::TrinoServer server(FLAGS_etc_dir);
+  server.run();
+  LOG(INFO) << "SHUTDOWN: Exiting main()";
+}
 
 // Initialize singleton for the reporter
 folly::Singleton<facebook::velox::BaseStatsReporter> reporter([]() {
-    return new facebook::velox::DummyStatsReporter();
+  return new facebook::velox::DummyStatsReporter();
 });
