61e04fee884a864306b6c137871e9c42b62d6a7d
Xuedong Luan <luanxuedong2009@gmail.com>
2022-07-22 17:02:19 +0800

    Update project namespace
    * update namespace phase one
    
    * update namespace phase two
    
    * update namespace phase three

--- src/trino/server/TaskManager.h
+++ src/trino/server/TaskManager.h
@@ -1,115 +1,144 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #pragma once
 
-
-#pragma once
-
-#include <memory>
 #include <folly/Synchronized.h>
-#include "QueryContextManager.h"
-#include "TrinoTask.h"
+#include <velox/exec/PartitionedOutputBufferManager.h>
+#include <memory>
+
 #include "http/HttpServer.h"
 #include "protocol/TrinoProtocol.h"
-#include "velox/exec/PartitionedOutputBufferManager.h"
+#include "server/QueryContextManager.h"
+#include "server/TrinoTask.h"
 
-using namespace facebook;
+namespace datalight::trino {
 
-namespace datalight::server
-{
-struct DriverCountStats
-{
-    size_t numBlockedDrivers{0};
-    size_t numRunningDrivers{0};
+struct DriverCountStats {
+  size_t numBlockedDrivers{0};
+  size_t numRunningDrivers{0};
 };
 
-class TaskManager
-{
-public:
-    explicit TaskManager(
-        std::unordered_map<std::string, std::string> properties = {}, std::unordered_map<std::string, std::string> nodeProperties = {});
+class TaskManager {
+ public:
+  explicit TaskManager(
+      std::unordered_map<std::string, std::string> properties = {},
+      std::unordered_map<std::string, std::string> nodeProperties = {});
 
-    void setBaseUri(const std::string & baseUri) { baseUri_ = baseUri; }
+  void setBaseUri(const std::string& baseUri) {
+    baseUri_ = baseUri;
+  }
 
-    TaskMap tasks() const
-    {
-        return taskMap_.withRLock([](const auto & tasks) { return tasks; });
-    }
+  TaskMap tasks() const {
+    return taskMap_.withRLock([](const auto& tasks) { return tasks; });
+  }
 
-    void abortResults(const protocol::TaskId & taskId, long bufferId);
+  void abortResults(const protocol::TaskId& taskId, long bufferId);
 
-    void acknowledgeResults(const protocol::TaskId & taskId, long bufferId, long token);
+  void
+  acknowledgeResults(const protocol::TaskId& taskId, long bufferId, long token);
 
-    // Creating an empty task that only contains the error information so that
-    // next time coordinator checks for the status it retrieves the error.
-    std::unique_ptr<protocol::TaskInfo> createOrUpdateErrorTask(const protocol::TaskId & taskId, const std::exception_ptr & exception);
+  // Creating an empty task that only contains the error information so that
+  // next time coordinator checks for the status it retrieves the error.
+  std::unique_ptr<protocol::TaskInfo> createOrUpdateErrorTask(
+      const protocol::TaskId& taskId,
+      const std::exception_ptr& exception);
 
-    std::unique_ptr<protocol::TaskInfo> createOrUpdateTask(
-        const protocol::TaskId & taskId,
-        velox::core::PlanFragment planFragment,
-        //const std::vector<protocol::TaskSource> & sources,
-        const protocol::OutputBuffers & outputBuffers,
-        std::unordered_map<std::string, std::string> && configStrings,
-        std::unordered_map<std::string, std::unordered_map<std::string, std::string>> && connectorConfigStrings);
+  std::unique_ptr<protocol::TaskInfo> createOrUpdateTask(
+      const protocol::TaskId& taskId,
+      facebook::velox::core::PlanFragment planFragment,
+      const std::vector<protocol::TaskSource>& sources,
+      const protocol::OutputBuffers& outputBuffers,
+      std::unordered_map<std::string, std::string>&& configStrings,
+      std::unordered_map<
+          std::string,
+          std::unordered_map<std::string, std::string>>&&
+          connectorConfigStrings);
 
-    std::unique_ptr<protocol::TaskInfo> deleteTask(const protocol::TaskId & taskId, bool abort);
+  std::unique_ptr<protocol::TaskInfo> deleteTask(
+      const protocol::TaskId& taskId,
+      bool abort);
 
-    /// Remove old Finished, Cancelled, Failed and Aborted tasks.
-    /// Old is being defined by the lifetime of the task.
-    size_t cleanOldTasks();
+  /// Remove old Finished, Cancelled, Failed and Aborted tasks.
+  /// Old is being defined by the lifetime of the task.
+  size_t cleanOldTasks();
 
-    folly::Future<std::unique_ptr<protocol::TaskInfo>> getTaskInfo(
-        const protocol::TaskId & taskId,
-        bool summarize,
-        std::optional<protocol::TaskState> currentState,
-        std::optional<protocol::Duration> maxWait,
-        std::shared_ptr<http::CallbackRequestHandlerState> state);
+  folly::Future<std::unique_ptr<protocol::TaskInfo>> getTaskInfo(
+      const protocol::TaskId& taskId,
+      bool summarize,
+      std::optional<protocol::TaskState> currentState,
+      std::optional<protocol::Duration> maxWait,
+      std::shared_ptr<http::CallbackRequestHandlerState> state);
 
-    folly::Future<std::unique_ptr<Result>> getResults(
-        const protocol::TaskId & taskId,
-        long bufferId,
-        long token,
-        protocol::DataSize maxSize,
-        protocol::Duration maxWait,
-        std::shared_ptr<http::CallbackRequestHandlerState> state);
+  folly::Future<std::unique_ptr<Result>> getResults(
+      const protocol::TaskId& taskId,
+      long bufferId,
+      long token,
+      protocol::DataSize maxSize,
+      protocol::Duration maxWait,
+      std::shared_ptr<http::CallbackRequestHandlerState> state);
 
-    folly::Future<std::unique_ptr<protocol::TaskStatus>> getTaskStatus(
-        const protocol::TaskId & taskId,
-        std::optional<protocol::TaskState> currentState,
-        std::optional<protocol::Duration> maxWait,
-        std::shared_ptr<http::CallbackRequestHandlerState> state);
+  folly::Future<std::unique_ptr<protocol::TaskStatus>> getTaskStatus(
+      const protocol::TaskId& taskId,
+      std::optional<protocol::TaskState> currentState,
+      std::optional<protocol::Duration> maxWait,
+      std::shared_ptr<http::CallbackRequestHandlerState> state);
 
-    void removeRemoteSource(const protocol::TaskId & taskId, const protocol::TaskId & remoteSourceTaskId);
+  void removeRemoteSource(
+      const protocol::TaskId& taskId,
+      const protocol::TaskId& remoteSourceTaskId);
 
-    std::string toString() const;
+  std::string toString() const;
 
-    QueryContextManager * getQueryContextManager() { return &queryContextManager_; }
+  QueryContextManager* getQueryContextManager() {
+    return &queryContextManager_;
+  }
 
-    const QueryContextManager * getQueryContextManager() const { return &queryContextManager_; }
+  const QueryContextManager* getQueryContextManager() const {
+    return &queryContextManager_;
+  }
 
-    inline size_t getNumTasks() const { return taskMap_.rlock()->size(); }
+  inline size_t getNumTasks() const {
+    return taskMap_.rlock()->size();
+  }
 
-    // Returns the number of running drivers in all tasks.
-    DriverCountStats getDriverCountStats() const;
+  // Returns the number of running drivers in all tasks.
+  DriverCountStats getDriverCountStats() const;
 
-    // Returns array with number of tasks for each of five TaskState (enum defined
-    // in exec/Task.h).
-    std::array<size_t, 5> getTaskNumbers(size_t & numTasks) const;
+  // Returns array with number of tasks for each of five TaskState (enum defined
+  // in exec/Task.h).
+  std::array<size_t, 5> getTaskNumbers(size_t& numTasks) const;
 
-public:
-    static constexpr folly::StringPiece kMaxDriversPerTask{"max_drivers_per_task"};
-    static constexpr folly::StringPiece kConcurrentLifespansPerTask{"concurrent_lifespans_per_task"};
-    static constexpr folly::StringPiece kSessionTimezone{"session_timezone"};
+ public:
+  static constexpr folly::StringPiece kMaxDriversPerTask{
+      "max_drivers_per_task"};
+  static constexpr folly::StringPiece kConcurrentLifespansPerTask{
+      "concurrent_lifespans_per_task"};
+  static constexpr folly::StringPiece kSessionTimezone{"session_timezone"};
 
-private:
-    std::shared_ptr<TrinoTask> findOrCreateTask(const protocol::TaskId & taskId);
+ private:
+  std::shared_ptr<PrestoTask> findOrCreateTask(const protocol::TaskId& taskId);
 
-    std::shared_ptr<TrinoTask> findOrCreateTaskLocked(TaskMap & taskMap, const protocol::TaskId & taskId);
+  std::shared_ptr<PrestoTask> findOrCreateTaskLocked(
+      TaskMap& taskMap,
+      const protocol::TaskId& taskId);
 
-    std::string baseUri_;
-    std::shared_ptr<velox::exec::PartitionedOutputBufferManager> bufferManager_;
-    folly::Synchronized<TaskMap> taskMap_;
-    QueryContextManager queryContextManager_;
-    int32_t maxDriversPerTask_;
-    int32_t concurrentLifespansPerTask_;
+  std::string baseUri_;
+  std::shared_ptr<facebook::velox::exec::PartitionedOutputBufferManager> bufferManager_;
+  folly::Synchronized<TaskMap> taskMap_;
+  QueryContextManager queryContextManager_;
+  int32_t maxDriversPerTask_;
+  int32_t concurrentLifespansPerTask_;
 };
 
-}
+} // namespace datalight::trino
